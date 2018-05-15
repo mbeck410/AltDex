8c0889d6265db58fa93b1014aab9bf86b9121a44
MattB <mbeck410@gmail.com>
2018-05-15 14:41:04 -0700

    'deploying'

--- AltDex2/settings.py
+++ AltDex2/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'd$tbu2e=mu(8ntx8w!9^ll_s1g)(74w7ogu=5u3$m_59#e5z84'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost'
+    'localhost',
+    'altdex22-dev.us-west-2.elasticbeanstalk.com',
+    '127.0.0.1'
 ]
 
 
