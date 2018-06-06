865120fb9b1efabe6eb551f100c2d99fc7074d4f
MattB <mbeck410@gmail.com>
2018-06-06 16:57:34 -0700

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -111,19 +111,19 @@ def getcoinscurrent(request):
 
         for dex in indices_in:
             dices += str(dex.name)
-            if 'AltDex' in dex.name:
+            if 'AltDex' in indices_in:
                 weight_1 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
                 if weight_1 >= 1:
                     weight_1 = format(float(weight_1), '.2f')
                 else:
                     weight_1 = float('{0:.6f}'.format(weight_1))
-            if 'Privacy' in dex.name:
+            if 'Privacy' in indices_in:
                 weight_2 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
                 if weight_2 >= 1:
                     weight_2 = format(float(weight_2), '.2f')
                 else:
                     weight_2 = float('{0:.6f}'.format(weight_2))
-            if 'Exchange' in dex.name:
+            if 'Exchange' in indices_in:
                 weight_3 = this_coin.market_cap / dex.indexprice_set.last().market_cap * 100
                 if weight_3 >= 1:
                     weight_3 = format(float(weight_3), '.2f')
