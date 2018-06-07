7f00c100fe7e280dd5367f9ef814da94c66fcafe
MattB <mbeck410@gmail.com>
2018-06-07 15:56:16 -0700

    'deploy'

--- altdex/helpers.py
+++ altdex/helpers.py
@@ -89,11 +89,11 @@ def collect():
         dex_percent_change = 0
         amount_entries = len(dex.indexprice_set.all())
 
-        if amount_entries < 2189:
+        if amount_entries < 4053:
             this_change = 0
 
         else:
-            price_id = amount_entries - 2189
+            price_id = amount_entries - 4053
             last_price = dex.indexprice_set.all()[price_id].price
             this_change = float(dex_price) - float(last_price)
             dex_percent_change = (this_change/float(last_price)) * 100
