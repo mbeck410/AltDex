e53964a33c1cf361f7b57da05e8d262b3f518c95
MattB <mbeck410@gmail.com>
2018-06-19 18:35:57 -0700

    'deploy'

--- altdex/apps.py
+++ altdex/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class AltdexConfig(AppConfig):
     name = 'altdex'
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
