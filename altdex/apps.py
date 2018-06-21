186886abcd7a77a53c090e1b08cceb8e2d7c2579
MattB <mbeck410@gmail.com>
2018-06-20 18:21:20 -0700

    'deploy'

--- altdex/apps.py
+++ altdex/apps.py
@@ -5,6 +5,7 @@ class AltdexConfig(AppConfig):
     name = 'altdex'
 
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+
