7593a1d15eee4f6de3a5c679ebd2218d86e0e477
MattB <mbeck410@gmail.com>
2018-07-17 14:11:15 -0700

    'deploy'

--- AltDex2/settings/development.py
+++ AltDex2/settings/development.py
@@ -4,3 +4,5 @@ import os
 SECRET_KEY = 'd$tbu2e=mu(8ntx8w!9^ll_s1g)(74w7ogu=5u3$m_59#e5z84'
 
 DEBUG = True
+
+ALLOWED_HOSTS = ['*']
--- AltDex2/settings/production.py
+++ AltDex2/settings/production.py
@@ -3,4 +3,5 @@ import os
 
 DEBUG = False
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = ['*']
+
