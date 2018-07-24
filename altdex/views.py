c962a2465900aed94e5aa135a1790bc088d4e351
MattB <mbeck410@gmail.com>
2018-07-24 15:19:18 -0700

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -133,26 +133,26 @@ def getcoinscurrent(request):
             if dex.name == 'AltDex100':
                 weight_1 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
                 if weight_1 >= 1:
-                    weight_1 = format(float(weight_1), '.3f')
+                    weight_1 = float('{0:.3f}'.format(weight_1))
                 else:
                     weight_1 = float('{0:.6f}'.format(weight_1))
             if dex.name == 'Exchange':
                 weight_2 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
                 if weight_2 >= 1:
-                    weight_2 = format(float(weight_2), '.3f')
+                    weight_2 = float('{0:.3f}'.format(weight_2))
                 else:
                     weight_2 = float('{0:.6f}'.format(weight_2))
             if dex.name == 'Privacy':
                 weight_3 = this_coin.market_cap / dex.indexprice_set.last().market_cap * 100
                 if weight_3 >= 1:
-                    weight_3 = format(float(weight_3), '.3f')
+                    weight_3 = float('{0:.3f}'.format(weight_3))
                 else:
                     weight_3 = float('{0:.6f}'.format(weight_3))
 
         if float(this_coin.price) >= 1:
-            coin_price = '{:,.2f}'.format(float(this_coin.price))
+            coin_price = float('{:,.2f}'.format(this_coin.price))
         else:
-            coin_price = '{0:.6f}'.format(float(this_coin.price))
+            coin_price = float('{0:.6f}'.format(this_coin.price))
 
         name_lower = str(this_coin.name).lower()
         if ' ' in name_lower:
@@ -195,13 +195,13 @@ def getcoinscurrent(request):
                         'website': this_coin.website,
                         'symbol': this_coin.symbol,
                         'market_cap': float('{0:.0f}'.format(this_coin.market_cap)),
-                        'price': float(coin_price),
+                        'price': coin_price,
                         'price_percent': float('{0:.2f}'.format(this_coin.price_percent_change)),
                         'volume': float('{0:.0f}'.format(this_coin.volume)),
                         'indices': dices,
-                        'weight_1': float(weight_1),
-                        'weight_2': float(weight_2),
-                        'weight_3': float(weight_3),
+                        'weight_1': weight_1,
+                        'weight_2': weight_2,
+                        'weight_3': weight_3,
                         'icon': coin_icon_url
                         }
 
