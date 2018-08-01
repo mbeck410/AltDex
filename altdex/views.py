e3d3421b447e7f32aa719582234bf2d1955258ca
MattB <mbeck410@gmail.com>
2018-08-01 00:03:55 -0700

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -134,33 +134,36 @@ def getcoinscurrent(request):
         indices_in = this_coin.indices.all()
 
         for dex in indices_in:
-            if dex.name != 'Null':
-                dices += str(dex.name)
-                if dex.name == 'AltDex100':
-                    weight_1 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
-                    if weight_1 >= 1:
-                        weight_1 = float('{0:.3f}'.format(weight_1))
-                    else:
-                        weight_1 = float('{0:.6f}'.format(weight_1))
-                if dex.name == 'Exchange':
-                    weight_2 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
-                    if weight_2 >= 1:
-                        weight_2 = float('{0:.3f}'.format(weight_2))
-                    else:
-                        weight_2 = float('{0:.6f}'.format(weight_2))
-                if dex.name == 'Privacy':
-                    weight_3 = this_coin.market_cap / dex.indexprice_set.last().market_cap * 100
-                    if weight_3 >= 1:
-                        weight_3 = float('{0:.3f}'.format(weight_3))
-                    else:
-                        weight_3 = float('{0:.6f}'.format(weight_3))
 
-                if dex.name == 'Masternode':
-                    weight_4 = this_coin.market_cap / dex.indexprice_set.last().market_cap * 100
-                    if weight_4 >= 1:
-                        weight_4 = float('{0:.3f}'.format(weight_4))
-                    else:
-                        weight_4 = float('{0:.6f}'.format(weight_4))
+            if dex.name == 'AltDex100':
+                weight_1 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
+                dices += str(dex.name)
+                if weight_1 >= 1:
+                    weight_1 = float('{0:.3f}'.format(weight_1))
+                else:
+                    weight_1 = float('{0:.6f}'.format(weight_1))
+            if dex.name == 'Exchange':
+                weight_2 = this_coin.market_cap/dex.indexprice_set.last().market_cap * 100
+                dices += str(dex.name)
+                if weight_2 >= 1:
+                    weight_2 = float('{0:.3f}'.format(weight_2))
+                else:
+                    weight_2 = float('{0:.6f}'.format(weight_2))
+            if dex.name == 'Privacy':
+                weight_3 = this_coin.market_cap / dex.indexprice_set.last().market_cap * 100
+                dices += str(dex.name)
+                if weight_3 >= 1:
+                    weight_3 = float('{0:.3f}'.format(weight_3))
+                else:
+                    weight_3 = float('{0:.6f}'.format(weight_3))
+
+            if dex.name == 'Masternode':
+                weight_4 = this_coin.market_cap / dex.indexprice_set.last().market_cap * 100
+                dices += str(dex.name)
+                if weight_4 >= 1:
+                    weight_4 = float('{0:.3f}'.format(weight_4))
+                else:
+                    weight_4 = float('{0:.6f}'.format(weight_4))
 
         if float(this_coin.price) >= 1:
             coin_price = '{:,.2f}'.format(float(this_coin.price))
