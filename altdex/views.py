1103b38341750aa1728cd47a1b5cae968990b1a7
MattB <mbeck410@gmail.com>
2018-09-04 22:08:06 -0700

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 # from django.urls import reverse
 # from decimal import Decimal
-from datetime import datetime, minute, month, day, hour
+import datetime
 # from threading import Timer
 # from time import sleep
 # import schedule
