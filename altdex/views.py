5897878bf3577536ef85895bc90d77e4b7363a76
MattB <mbeck410@gmail.com>
2018-09-12 15:53:47 -0700

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -289,12 +289,21 @@ def getindexperformance(request):
                     month_index = i
                     break
 
+                if i > 35000:
+                    week_change = current_price - entries[week_index].price
+                    month_change = current_price - entries[month_index].price
+                    break
+
+            week_prices = entries[:week_index]
+            top_price = week_prices.latest('price')
+
             change_dict = {'seven': seven,
                             'week': week_change,
                             'month': one_m,
                             'month_change': month_change,
                             'week_index': week_index,
-                            'month_index':month_index
+                            'month_index':month_index,
+                            'top_price': top_price
                         }
 
             performance_table.append(change_dict)
