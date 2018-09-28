7fb6b9ac6efec4286233730f3de60df228216c01
MattB <mbeck410@gmail.com>
2018-09-28 16:38:00 -0700

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -422,7 +422,7 @@ def rsi_calc(request):
     smooth_rs = 0
     index = Index.objects.get(name="AltDex100")
     prices = index.indexprice_set.order_by('timestamp')
-    new_prices = prices.objects.filter(timestamp__hour=19)
+    new_prices = prices.filter(timestamp__hour=19)
     for price in new_prices:
         displayed_prices.append(price.timestamp)
     return JsonResponse({'prices': displayed_prices})
