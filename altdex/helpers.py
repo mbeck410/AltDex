42b4a510af1c9096fbeaab67426362caf0db9a0d
MattB <mbeck410@gmail.com>
2018-10-04 19:54:31 -0700

    'deploy'

--- altdex/helpers.py
+++ altdex/helpers.py
@@ -10,7 +10,7 @@ def collect():
     coins = Coin.objects.all()
     indices = Index.objects.all()
 
-    url1 = 'https://api.coincap.io/v2/assets?limit=2000'
+    url1 = 'https://api.coincap.io/v2/assets?limit=1000'
 
     r1 = requests.get(url1)
 
@@ -62,6 +62,24 @@ def collect():
 
         elif coin.symbol == 'CMT':
             url2 = 'https://api.coinmarketcap.com/v2/ticker/2246/'
+            r2 = requests.get(url2)
+
+            while r2.status_code != 200:
+                sleep(10)
+                r2 = requests.get(url2)
+
+            data = json.loads(r2.text)
+
+            coin.price = float(data['data']['quotes']['USD']['price'])
+            coin.price_percent_change = float('{0:.2f}'.format(data['data']['quotes']['USD']['percent_change_24h']))
+            coin.volume = float('{0:.0f}'.format(data['data']['quotes']['USD']['volume_24h']))
+            coin.market_cap = float('{0:.0f}'.format(data['data']['quotes']['USD']['market_cap']))
+            coin.percent_weight = 0
+
+            coin.save(update_fields=['price', 'price_percent_change', 'volume', 'market_cap', 'percent_weight'])
+
+        elif coin.symbol == 'CNX':
+            url2 = 'https://api.coinmarketcap.com/v2/ticker/2027/'
             r2 = requests.get(url2)
 
             while r2.status_code != 200:
