b02e60f7d9c6c43bcdb6ddd419518790d66a7a73
MattB <mbeck410@gmail.com>
2018-10-12 22:55:30 -0700

    'deploy'

--- altdex/admin.py
+++ altdex/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Index, Coin, IndexPrice, IndexDay
+from .models import Index, Coin, IndexPrice, IndexDay, Test
 
 admin.site.register(Index)
 admin.site.register(IndexPrice)
 admin.site.register(Coin)
 admin.site.register(IndexDay)
+admin.site.register(Test)
