26652d6019b5b38c110090bc3975d82a9e138cc7
MattB <mbeck410@gmail.com>
2018-10-12 19:10:58 -0700

    'deploy'

--- altdex/management/commands/start_indices.py
+++ altdex/management/commands/start_indices.py
@@ -12,15 +12,13 @@ class Command(BaseCommand):
         # print('Do you wish to delete all index prices?')
         # response = input('y or n?: ')
         # if response is 'y':
-        try:
-            # print('Collecting...')
-            test = rsi_calc_init()
-            # print('Setting prices...')
-            # test2 = first_weight()
-            print('Done')
+        while True:
+            try:
+                test = rsi_calc_init()
+                print('Done')
 
-        except:
-            raise CommandError("Error")
+            except:
+                raise CommandError("Error")
 
         # else:
         #     return
