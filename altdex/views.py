4927587ce12be87cdfe1cbab8bcd7dff40d01ce2
MattB <mbeck410@gmail.com>
2019-04-01 01:09:07 -0400

    'deploy'

--- altdex/views.py
+++ altdex/views.py
@@ -199,22 +199,40 @@ def getgamingindex(request):
 
 
 def getgamingcurrent(request):
-    dex = Index.objects.get(name='Gaming')
+    indices = Index.objects.order_by('id')
     indices_current = []
-    dex_current = dex.indexprice_set.last()
-    link = '/gaming'
-    symbol = 'ALTGME'
-    index_dict = {  'link': link,
-                    'name': dex.name,
-                    'price': float('{0:.2f}'.format(dex_current.price)),
-                    'change_24h': float('{0:.2f}'.format(dex_current.change_24h)),
-                    'price_percent': float('{0:.2f}'.format(dex_current.price_percent_change)),
-                    'market_cap': float('{0:.0f}'.format(dex_current.market_cap)),
-                    'time': str(dex_current.timestamp),
-                    'symbol': symbol
-                }
 
-    indices_current.append(index_dict)
+    for dex in indices:
+        if dex.name != 'Null':
+            link = ''
+            if dex.name == 'AltDex100':
+                link = '/alt100'
+                symbol = 'ALT100'
+            elif dex.name == 'Exchange':
+                link = '/exchange'
+                symbol = 'ALTEXC'
+            elif dex.name == 'Privacy':
+                link = '/privacy'
+                symbol = 'ALTPRV'
+            elif dex.name == 'Masternode':
+                link = '/masternode'
+                symbol = 'ALTMSN'
+            elif dex.name == 'Gaming':
+                link = '/gaming'
+                symbol = 'ALTGME'
+
+            dex_current = dex.indexprice_set.last()
+            index_dict = {  'link': link,
+                            'name': dex.name,
+                            'price': float('{0:.2f}'.format(dex_current.price)),
+                            'change_24h': float('{0:.2f}'.format(dex_current.change_24h)),
+                            'price_percent': float('{0:.2f}'.format(dex_current.price_percent_change)),
+                            'market_cap': float('{0:.0f}'.format(dex_current.market_cap)),
+                            'time': str(dex_current.timestamp),
+                            'symbol': symbol
+                        }
+
+            indices_current.append(index_dict)
 
     return JsonResponse({'dict_key': indices_current})
 
